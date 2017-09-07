d9248c602fdf1f851d8af37698f0b51c3f7ed844
Michal Janek <mikael.janek@gmail.com>
2017-09-07 03:41:33 +0200

    add randomuser API

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-
+    const $gallery = $("#gallery");
+    const randomAPI = 'https://randomuser.me/api/?results=9';
+    const displayProfile = (data) => {
+        console.log(data);
+        $.each(data.results, (i,user)=>{
+            const $img = $("<img class='profile-image'src='"+user.picture.thumbnail+"'/>");
+            // const $img = $(`<img class='profile-image'src='${user.picture.thumbnail}'/>`); not supported in IE11
+            $gallery.append($img);
+            $("<h1 class='profile-text'>"+ user.name.first +" "+user.name.last  + "</h1>").appendTo($gallery);
+        });
+    };
     $.ajax({
-        url: 'https://randomuser.me/api/',
+        url: randomAPI,
         dataType: 'json',
-        success: function (data) {
-            console.log(data);
-        }
+        success: displayProfile
     });
 });
