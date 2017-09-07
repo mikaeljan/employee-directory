b79af4c9bceaa9f7bea126d25f1a301a7a835683
Michal Janek <mikael.janek@gmail.com>
2017-09-07 15:11:10 +0200

    add styling to gallery and profiles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
+    // Function for capitalizing first letter of the string.
+    function titleCase(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     const $gallery = $("#gallery");
-    const randomAPI = 'https://randomuser.me/api/?results=9';
+    const randomAPI = 'https://randomuser.me/api/?results=10';
     const displayProfile = (data) => {
         console.log(data);
         $.each(data.results, (i,user)=>{
-            const $img = $("<img class='profile-image'src='"+user.picture.thumbnail+"'/>");
             // const $img = $(`<img class='profile-image'src='${user.picture.thumbnail}'/>`); not supported in IE11
-            $gallery.append($img);
-            $("<h1 class='profile-text'>"+ user.name.first +" "+user.name.last  + "</h1>").appendTo($gallery);
+            const $profile = $("<div class='profile'></div>");
+            const $img = $("<img class='profile-image'src='"+user.picture.medium+"'/>");
+            const $profileText = $("<div class='profile-text'></div>");
+
+            $gallery.append($profile);
+            $profile.append($img);
+            $profile.append($profileText);
+
+            const profileName = titleCase(user.name.first)+" "+titleCase(user.name.last);
+            const profileEmail = user.email;
+            const profileCity = titleCase(user.location.city);
+
+            const $profileName = $("<h2 class='profile-text--name'></h2>");
+            const $profileEmail = $("<a class='profile-text--email' href='mailto:"+user.email+"'></a>");
+            const $profileCity = $("<p class='profile-text--city'></p>");
+
+            $profileName.text(profileName);
+            $profileEmail.text(profileEmail);
+            $profileCity.text(profileCity);
+
+            $profileText.append($profileName).append($profileEmail).append($profileCity);
+
+
         });
     };
     $.ajax({
