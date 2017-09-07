64e7fe066b635675f1d32b439aac3c1c791ef03f
Michal Janek <mikael.janek@gmail.com>
2017-09-07 02:03:12 +0200

    add more components

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+        }
+    });
+});
